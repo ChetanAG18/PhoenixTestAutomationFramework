8268bab7e5192e6a3b0606ab3635cca753d33643
Chetan AG <agchetan18@gmail.com>
2026-01-23 16:09:06 +0530

    updated variable name: DB_USER_NAME to DB_USERNAME

--- src/test/java/com/database/DatabaseManager.java
+++ src/test/java/com/database/DatabaseManager.java
@@ -11,7 +11,7 @@ import com.zaxxer.hikari.HikariDataSource;
 public class DatabaseManager {
 
 	private static final String DB_URL = EnvUtil.getValue("DB_URL");
-	private static final String DB_USER_NAME = EnvUtil.getValue("DB_USER_NAME");
+	private static final String DB_USER_NAME = EnvUtil.getValue("DB_USERNAME");
 	private static final String DB_PASSWORD = EnvUtil.getValue("DB_PASSWORD");
 	private static final int MAXIMUM_POOL_SIZE = Integer.parseInt(ConfigManager.getProperty("MAXIMUM_POOL_SIZE"));
 	private static final int MINIMUM_IDLE_COUNT = Integer.parseInt(ConfigManager.getProperty("MINIMUM_IDLE_COUNT"));
