cc24abf62d1c9b7edc72b8ac5f32e0d65b743c72
Chetan AG <agchetan18@gmail.com>
2026-01-23 13:46:35 +0530

    Created EnvUtil to read .env file and get value of the variable.
    Modified the DatabaseManager Class to read the JDBC URL and Credentials
    from the .env

--- src/test/java/com/api/tests/CreateJobAPIWithDBValidationTest.java
+++ src/test/java/com/api/tests/CreateJobAPIWithDBValidationTest.java
@@ -52,7 +52,7 @@ public class CreateJobAPIWithDBValidationTest {
 	public void setUp() {
 		customer = new Customer("Chetan", "AG", "7090191744", "", "agchetan18@gmail.com", "");
 		customerAddress = new CustomerAddress("D 404", "Vasant Galaxy", "Bangaru nagar", "Inorbit", "Mumbai", "411039", "India", "Maharashtra");
-		customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "78920578946787", "78920578946787", "78920578946787", getTimeWithDaysAgo(10),
+		customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "78920578946878", "78920578946878", "78920578946878", getTimeWithDaysAgo(10),
 				Product.NEXUS_2.getCode(), Model.NEXUS_2_BLUE.getCode());
 		
 		Problems problems = new Problems(Problem.SMARTPHONE_IS_RUNNING_SLOW.getCode(), "Battery Issue");
